b2a92d9d926cff43cf907d515801c8367232e87b
Rohit Prasad <rohitrp3@gmail.com>
2016-02-01 23:00:42 +0530

    Fix search bug

--- public/script.js
+++ public/script.js
@@ -1,33 +1,38 @@
 $(document).ready(function () {
   $('.loading').hide();
+
+  function getResults() {
+    $('.loading').show()
+    var search = $(this).val()
+    $('body').append("<h1>searching " + search + "</h1>")
+    
+    $.ajax({
+      url: '/scrape?q=' + search,
+      dataType: 'json',
+      complete: function (res, status) {
+        $('.search .loading').hide()
+      },
+      success: function (data) {
+        $('body').append("<p>" + JSON.stringify(data) + "</p>")
+      },
+      error: function (res) {
+        $('body').append('<h1>ERROR</h1>')
+      }
+    })
+  }
   
   $('input').keypress(function (e) {
     if (e.which == 13) {
-      $('.loading').show()
-      var search = $(this).val()
-      $('body').append("<h1>searching " + search + "</h1>")
-      $.ajax({
-        url: '/scrape?q=' + search,
-        dataType: 'json',
-        complete: function (res, status) {
-          $('.search .loading').hide()
-        },
-        success: function (data) {
-          $('body').append("<p>" + JSON.stringify(data) + "</p>")
-        },
-        error: function (res) {
-          $('body').append('<h1>ERROR</h1>')
-        }
-      })
+      getResults()
 
       return false
     }
   })
-  
+
   function adjustSearchBar() {
     var width = "300px";
     var borderColor = "#3F51B5";
-    
+
     if ($("#search-input").val() == "") {
       width = "0px";
       borderColor = "#fff";
@@ -37,13 +42,15 @@ $(document).ready(function () {
     $('#search-input').css('border-color', borderColor);
   };
 
-  $('.search i').click(function() {
+  $('.search i').click(function () {
     $('#search-input').css('width', '300px')
     $('#search-input').css('border-color', '#3F51B5')
     $('#search-input').focus()
+    
+    if ($('#search-input').val() != '') getResults()
   })
-  
-  $('html').click(function(e) {
+
+  $('html').click(function (e) {
     var target = $(e.target)
     if (!target.is($(".search i")) && !target.is($('#search-input'))) {
       adjustSearchBar();
