b7cd3d7bb486c928072b0038bf65e13aae5e8735
Rohit Prasad <rohitrp3@gmail.com>
2016-02-02 00:48:56 +0530

    Provide basic front end for users

--- public/script.js
+++ public/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
   $('.loading').hide();
-
+  $('.products').hide()
   function getResults() {
+    $('.products').hide()
+    
     $('.loading').show()
     var search = $('#search-input').val()
-    $('body').append("<h1>searching " + search + "</h1>")
-    
+
     $.ajax({
       url: '/scrape?q=' + search,
       dataType: 'json',
@@ -13,14 +14,36 @@ $(document).ready(function () {
         $('.search .loading').hide()
       },
       success: function (data) {
-        $('body').append("<p>" + JSON.stringify(data) + "</p>")
+        console.log(data)
+        var flipkart = data.products.flipkart,
+          snapdeal = data.products.snapdeal,
+          $flipkart = $('.products .flipkart'),
+          $snapdeal = $('.products .snapdeal')
+
+        $flipkart.find('.image').css('background-image', 'url('+flipkart.image+')')
+        $flipkart.find('.price h4').text(flipkart.price)
+        $flipkart.find('.image img').attr('alt', flipkart.title)
+        $flipkart.find('.title a').attr('href', flipkart.link)
+        $flipkart.find('.title h3').text(flipkart.title)
+        $flipkart.find('.ratings span').html(flipkart.ratings + " &#127775;")
+        $flipkart.find('.reviews span').text(flipkart.reviews + " reviews")
+
+        $snapdeal.find('.image').css('background-image', 'url('+snapdeal.image+')')
+        $snapdeal.find('.price h4').text(snapdeal.price)
+        $snapdeal.find('.image img').attr('alt', snapdeal.title)
+        $snapdeal.find('.title a').attr('href', snapdeal.link)
+        $snapdeal.find('.title h3').text(snapdeal.title)
+        $snapdeal.find('.ratings span').html(snapdeal.ratings + " &#127775;")
+        $snapdeal.find('.reviews span').text(snapdeal.reviews + " reviews")
+        
+        $('.products').show()
       },
       error: function (res) {
-        $('body').append('<h1>ERROR</h1>')
+        $('body').append('<h1>Error</h1>')
       }
     })
   }
-  
+
   $('input').keypress(function (e) {
     if (e.which == 13) {
       getResults()
@@ -46,7 +69,7 @@ $(document).ready(function () {
     $('#search-input').css('width', '300px')
     $('#search-input').css('border-color', '#3F51B5')
     $('#search-input').focus()
-    
+
     if ($('#search-input').val() != '') getResults()
   })
 
